4879cc9bddc42e651f28d66bbcc86bef2bf14ae5
Bhanu Priya Dixit <92316465+Bhanupriya013@users.noreply.github.com>
2024-06-24 10:42:39 +0530

    Update Orders

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+{
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+{
+    productName: 'LARVENDER KF102 Drone',
+    productNumber: '36378',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+},
+{
+    productName: 'Ruko F11 Pro Drone',
+    productNumber: '45862',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+{
+    productName: 'Rent DJI Mini 2 4K drone',
+    productNumber: '84581',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+},
+{
+    productName: 'GPS 4k Drone',
+    productNumber: '22855',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+},
+{
+    productName: 'DJI Air 25',
+    productNumber: '25641',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+{
+    productName: 'Lozenge Drone',
+    productNumber: '02561',
+    paymentStatus: 'Returned',
+    shipping: 'Declined'
+},
+]
